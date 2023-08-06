d592cfa2977f1728d1f4ae5c9014cd35a7e4338f
animegit <ajitanimesh1234@gmail.com>
2023-08-06 16:58:33 +0530

    Normalize URLS form HTML Body and tests

--- crawl.js
+++ crawl.js
@@ -1,4 +1,5 @@
 const { url } = require("inspector");
+const{JSDOM}=require('jsdom');
 
 function normalizeUrl(URLString){
    const urlobj=new URL(URLString);
@@ -10,6 +11,40 @@ function normalizeUrl(URLString){
   return hostpath;
 
 }
+
+function getURLFromHTML(htmlBody,baseURL){
+    const URLS=[]
+    const dom=new JSDOM(htmlBody);
+    const links=dom.window.document.querySelectorAll('a')
+    for(const link of links){
+        if(link.href.slice(0,1)=='/'){
+            //relative
+           
+            try{
+                const urlobj=new URL(`${baseURL}${link.href}`)
+                URLS.push(urlobj.href)
+            }
+            catch(err){
+                console.log(`Erorr normalizing relative url: ${err.message}`)
+            }
+
+            
+        }
+        else{
+        try{
+            const urlobj=new URL(link.href)
+            URLS.push(urlobj.href)
+        }
+        catch(err){
+            console.log(`Erorr normalizing absolute url: ${err.message}`)
+        }}
+        
+    }
+    return URLS;
+
+}
+
 module.exports={
     normalizeUrl,
+    getURLFromHTML,
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeUrl}=require("./crawl.js");
+const {normalizeUrl, getURLFromHTML}=require("./crawl.js");
 const{test,expect}=require("@jest/globals");
 
 test('normalizeUrl strip protocols',()=>{
@@ -27,6 +27,60 @@ test('normalizeUrl http',()=>{
     const input='http://MSRIT.edu/exams/';
     const actual=normalizeUrl(input);
     const expected='msrit.edu/exams';
+    expect(actual).toEqual(expected)
+
+})
+
+test('normalizeUrl HTMLBODY',()=>{
+    const inputHTMLBody = `
+<html>
+<body>
+    <a href="https://msrit.edu/">
+msrit edu
+   </a>
+</body>
+</html>
+`
+
+const baseURL='https://msrit.edu'
+    const actual=getURLFromHTML(inputHTMLBody,baseURL);
+    const expected=['https://msrit.edu/'];
+    expect(actual).toEqual(expected)
+
+})
+
+test('normalizeUrl relative',()=>{
+    const inputHTMLBody = `
+<html>
+<body>
+    <a href="/path/">
+msrit edu
+   </a>
+</body>
+</html>
+`
+
+const baseURL='https://msrit.edu'
+    const actual=getURLFromHTML(inputHTMLBody,baseURL);
+    const expected=['https://msrit.edu/path/'];
+    expect(actual).toEqual(expected)
+
+})
+
+test('normalizeUrl invalid',()=>{
+    const inputHTMLBody = `
+<html>
+<body>
+    <a href="invalid">
+msrit edu
+   </a>
+</body>
+</html>
+`
+
+const baseURL='https://msrit.edu'
+    const actual=getURLFromHTML(inputHTMLBody,baseURL);
+    const expected=[];
     expect(actual).toEqual(expected)
 
 })
